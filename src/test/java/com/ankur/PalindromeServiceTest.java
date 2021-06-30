a2cf3d5dfe1f72d339f8140a20b603d47e34c110
Ankur Israni <ankur.israni@aa.com>
2021-06-30 01:15:14 -0500

    refactored

--- src/test/java/com/ankur/PalindromeServiceTest.java
+++ src/test/java/com/ankur/PalindromeServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.Assert;
 import org.junit.Before;
 
 
-public class PalindromeFeatureTest {
+public class PalindromeServiceTest {
 
     private String input;
     private boolean isPalindrome;
